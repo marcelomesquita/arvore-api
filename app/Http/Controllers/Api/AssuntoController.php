69280544c7c5b95042a54623ec289686a55333dd
Marcelo Mesquita <contato@marcelomesquita.com>
2020-08-17 14:09:01 -0300

    alteracao dos atributos de retorno

--- app/Http/Controllers/Api/AssuntoController.php
+++ app/Http/Controllers/Api/AssuntoController.php
@@ -11,9 +11,9 @@ class AssuntoController extends Controller
     function pesquisar($idPai = null)
     {
         try {
-            return response()->json(['success' => Assunto::where('id_pai', $idPai)->withCount('filhos')->get()]);
+            return response()->json(['assuntos' => Assunto::where('id_pai', $idPai)->withCount('filhos')->get()]);
         } catch(\Exception $e) {
-           return response()->json(['failure' => 'Erro ao processar a requisição']);
+           return response()->json(['message' => 'Erro ao processar a requisição']);
         }
     }
 }
