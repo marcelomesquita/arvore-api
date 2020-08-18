a2af410aa18c2eccc1d20580531404167e72a739
Marcelo Mesquita <contato@marcelomesquita.com>
2020-08-18 15:11:32 -0300

    inclusao do filtro de assunto

--- app/Http/Controllers/Api/AssuntoController.php
+++ app/Http/Controllers/Api/AssuntoController.php
@@ -8,26 +8,29 @@ use Illuminate\Http\Request;
 
 class AssuntoController extends Controller
 {
-    function pesquisar()
+    function pesquisar(Request $request)
     {
         try {
+            $search = $request->all();
+
             $assuntos = Assunto::where('assunto_id', null)
                 ->with('assuntos')
-                // ->where('questoes.banca_id', 2)
-                // ->orWhere('assuntos.questoes.banca_id', 2)
-                // ->whereHas('questoes', function ($query) {
-                    // $query->where('banca_id', 2);
-                // })
-                // ->whereHas('assuntos', function ($query) {
-                //     $query->whereHas('questoes', function ($query) {
-                //         $query->where('banca_id', 2);
-                //     });
-                // })
+                ->whereHas('assuntos', function ($query) use ($search) {
+                    $query->whereHas('questoes', function ($query) use ($search) {
+                        if (isset($search['banca_id']) && !empty($search['banca_id'])) {
+                            $query->where('banca_id', $search['banca_id']);
+                        }
+
+                        if (isset($search['orgao_id']) && !empty($search['orgao_id'])) {
+                            $query->where('orgao_id', $search['orgao_id']);
+                        }
+                    });
+                })
                 ->get();
 
-            return response()->json(['assuntos' => $assuntos]);
+            return response()->json(['assuntos' => $assuntos], 200);
         } catch(\Exception $e) {
-           return response()->json(['message' => 'Erro ao processar a requisição'], 500);
+            return response()->json(['mensagem' => 'Erro ao processar a requisição: ' . $e->getMessage()], 500);
         }
     }
 }
